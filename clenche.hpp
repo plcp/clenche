9a5bd9cf4cf525e33eb50121eb7c2f4a2ad61906
plcp <matthieu@daumas.me>
2017-07-31 11:17:09 +0200

    Refactor + Get rid of lambda

--- clenche.hpp
+++ clenche.hpp
@@ -17,7 +17,7 @@ namespace cl
         using ftype = void(t_functor::*)(t_args...);
 
         template<class t_functor, typename... t_args>
-        auto sign(ftype<t_functor, int&, t_args...> foo __attribute__((unused)))
+        auto callee_details(ftype<t_functor, int&, t_args...>)
         {
             return cl::callee<t_args...>();
         }
@@ -26,38 +26,47 @@ namespace cl
         struct extract_callee
         {
             using type = decltype(
-                sign<t_functor>(&t_functor::template operator()<int>));
+                callee_details<t_functor>(
+                    &t_functor::template operator()<int>));
         };
 
         template<class t_functor>
         using callee = typename extract_callee<t_functor>::type;
     }
 
-    template<typename t_visitor, class t_functor>
-    struct wrapper : t_functor
+    template<typename t_visitor, class t_functor, typename... t_args>
+    struct wrapper;
+
+    template<typename t_visitor, class t_functor, typename... t_args>
+    struct wrapper<t_visitor, t_functor, callee<t_args...>>
+        : t_functor
     {
-        using callee_type = traits::callee<t_functor>;
+        using callee_type = callee<t_args...>;
+        void operator()(t_args&... args)
+        {
+            static_cast<t_functor&>(*this)(
+                static_cast<t_visitor&>(*this).machine, args...);
+        }
+
         void operator()(callee_type& callee)
         {
-            std::apply(
-                [this](auto&&... args) -> void
-                {
-                    static_cast<t_functor&>(*this)(
-                        static_cast<t_visitor*>(this)->machine, args...);
-                }, callee);
+            std::apply(*this, callee);
         }
     };
 
     template<typename t_machine, class... t_functors>
-    struct visitor : wrapper<visitor<t_machine, t_functors...>, t_functors>...
+    struct visitor
+        : wrapper<visitor<t_machine, t_functors...>, t_functors,
+            traits::callee<t_functors>>...
     {
         using machine_type = t_machine;
-        visitor(machine_type& machine) : machine(machine) { }
+        visitor(machine_type& machine)
+            : machine(machine) { }
         machine_type& machine;
 
         template<class t_functor>
-        using wrapper_type =
-            wrapper<visitor<machine_type, t_functors...>, t_functor>;
+        using wrapper_type = wrapper<visitor<machine_type, t_functors...>,
+            t_functor, traits::callee<t_functor>>;
         using wrapper_type<t_functors>::operator()...;
     };
 
@@ -68,7 +77,8 @@ namespace cl
         using visitor_type = visitor<machine<t_functors...>, t_functors...>;
 
         template<typename... t_args>
-        machine(t_args&&... args) : dispatcher(*this)
+        machine(t_args&&... args)
+            : dispatcher(*this)
         {
             prepare(args...);
         }
