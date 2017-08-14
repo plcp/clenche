9d7d70fc02a3fe3a3c25cabe1a899101c010d364
plcp <matthieu@daumas.me>
2017-08-14 16:07:00 +0200

    Use cl::edge to build cl::compose

--- src/sequence.hpp
+++ src/sequence.hpp
@@ -45,24 +45,6 @@ namespace cl::sequence
                     std::forward<t_args>(args)...);
             }
         };
-
-        template<typename t_first, typename... t_args>
-        struct wrap_functor<t_first, cl::none, cl::callee<t_args...>>
-            : t_first, cl::enable<
-                wrap_functor<t_first, cl::none, cl::callee<t_args...>>>
-        {
-            using functor_type = t_first;
-            using before = typename functor_type::before;
-            using after = typename functor_type::after;
-            using tag = typename functor_type::tag;
-
-            template<typename t_machine>
-            void operator()(t_machine& machine, t_args&&... args)
-            {
-                functor_type::operator()(machine,
-                    std::forward<t_args>(args)...);
-            }
-        };
     }
 
     namespace traits
@@ -90,63 +72,7 @@ namespace cl::sequence
             typename fix_compose_details<t_functors...>::type;
     }
 
-    template<typename t_first, typename t_second, typename... t_remaining>
-    struct compose_details;
-
-    template<typename t_first, typename t_second>
-    struct compose_details<t_first, t_second>
-    {
-        using type = cl::traits::pack<
-            details::wrap_functor<
-                t_first, t_second, cl::traits::callee_untagged<t_first>>,
-            details::wrap_functor<
-                t_second, cl::none, cl::traits::callee_untagged<t_second>>>;
-    };
-
-    template<typename t_first, typename t_second, typename... t_remaining>
-    struct compose_details
-        : compose_details<t_second, t_remaining...>
-    {
-        using type = typename cl::traits::merge_pack<
-            details::wrap_functor<
-                t_first, t_second, cl::traits::callee_untagged<t_first>>,
-            typename compose_details<t_second, t_remaining...>::type>::type;
-    };
-
-    // chain multiple functors together via calling prepare<next> in ::before
-    template<typename... t_functors>
-    using compose = typename compose_details<t_functors...>::type;
-
-    template<typename t_machine, typename t_pack>
-    struct machine_compose_details;
-
-    template<typename t_machine, typename... t_functors>
-    struct machine_compose_details<t_machine, cl::traits::pack<t_functors...>>
-        : cl::machine_details<t_machine, t_functors...>
-    {
-        using machine_type = t_machine;
-        using details_type = cl::machine_details<machine_type, t_functors...>;
-
-        template<typename... t_args>
-        machine_compose_details(t_args&&... args)
-            : details_type(std::forward<t_args>(args)...)
-        { }
-    };
-
-    template<typename t_machine, typename... t_functors>
-    using machine_details =
-        machine_compose_details<t_machine, traits::fix_compose<t_functors...>>;
-
-    // the only thing you may need to construct
-    template<typename... t_functors>
-    struct machine
-        : machine_details<machine<t_functors...>, t_functors...>
-    {
-        using machine_type = machine<t_functors...>;
-        using details_type = typename machine_details<
-                machine_type, t_functors...>::details_type;
-    };
-
+    // (specialized here and in property.hpp)
     template<
         typename t_dock,
         typename t_functor,
@@ -181,12 +107,71 @@ namespace cl::sequence
         }
     };
 
+    // Wraps t_functor and prepare a deferred call to t_next before its
+    // execution, does not remove any before/after preprocessing, but does hide
+    // (for now) the original functor. TOFIX
     template<typename t_functor, typename t_next>
     using edge = edge_details<
         cl::traits::dock<t_functor>,
         t_functor,
         t_next,
         cl::traits::callee_untagged<t_functor>>;
+
+    template<typename t_first, typename t_second, typename... t_remaining>
+    struct compose_details;
+
+    template<typename t_first, typename t_second>
+    struct compose_details<t_first, t_second>
+    {
+        using type = cl::traits::pack<edge<t_first, t_second>, t_second>;
+    };
+
+    template<typename t_first, typename t_second, typename... t_remaining>
+    struct compose_details
+        : compose_details<t_second, t_remaining...>
+    {
+        using type = typename cl::traits::merge_pack<
+            edge<t_first, t_second>,
+            typename compose_details<t_second, t_remaining...>::type>::type;
+    };
+
+    // chain multiple functors together via pairing each one except the last
+    // in a cl::edge, forming a default-sequence of deferred calls
+    template<typename... t_functors>
+    using compose = typename compose_details<t_functors...>::type;
+
+    template<typename t_machine, typename t_pack>
+    struct machine_compose_details;
+
+    template<typename t_machine, typename... t_functors>
+    struct machine_compose_details<t_machine, cl::traits::pack<t_functors...>>
+        : cl::machine_details<t_machine, t_functors...>
+    {
+        using machine_type = t_machine;
+        using details_type = cl::machine_details<machine_type, t_functors...>;
+
+        template<typename... t_args>
+        machine_compose_details(t_args&&... args)
+            : details_type(std::forward<t_args>(args)...)
+        { }
+    };
+
+    template<typename t_machine, typename... t_functors>
+    using machine_details =
+        machine_compose_details<t_machine, traits::fix_compose<t_functors...>>;
+
+    // machine type you have to use to enables sequence-related concepts
+    //  - provides regular behaviors (cl::machine)
+    //  - provides sequence behaviors (cl::sequence::machine)
+    //
+    template<typename... t_functors>
+    struct machine
+        : machine_details<machine<t_functors...>, t_functors...>
+    {
+        using machine_type = machine<t_functors...>;
+        using details_type = typename machine_details<
+                machine_type, t_functors...>::details_type;
+    };
 }
 
 namespace cl
